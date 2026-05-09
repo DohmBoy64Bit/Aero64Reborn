8d4c6753449eb110b3129be196a810e0226beed3
NomadWithoutAHome <96033888+NomadWithoutAHome@users.noreply.github.com>
2026-05-09 12:29:56 -0400

    "Fix-entrypoint-address-0x80200050-overlay-mapping"

--- recomp/lookup.cpp
+++ recomp/lookup.cpp
@@ -1,6 +1,6 @@
 #include "recomp.h"
 
-gpr get_entrypoint_address() { return (gpr)(int32_t)0x8023169Cu; }
+gpr get_entrypoint_address() { return (gpr)(int32_t)0x80200050u; }
 
 const char* get_rom_name() { return "aerofightersassaul.z64"; }
 
